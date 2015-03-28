12c8b3e882f664cd299fd3221692a33859f33d21
Jeffrey04 <mycyberpet@yahoo.com>
2015-03-28 12:56:09 +0800

    first attempt in solving, exported from 4clojure

--- problems/medium/55_count_occurences.clj
+++ problems/medium/55_count_occurences.clj
@@ -6,3 +6,12 @@
 ; (= (__ [:b :a :b :a :b]) {:a 2, :b 3})
 ; (= (__ '([1 2] [1 3] [1 3])) {[1 2] 1, [1 3] 2})
 
+(fn [i-list]
+    (let [unique-list (reduce conj #{} i-list)]
+        (apply hash-map
+            (interleave
+                unique-list
+                (map
+                    (fn [element]
+                        (count (filter #(= element %) i-list)))
+                    unique-list)))))
